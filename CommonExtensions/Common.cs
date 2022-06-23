876317c31848aafb6bd45cca5b1f47c01844c7d2
Lukas Nagel <lukas.nagel04@protonmail.com>
2022-06-23 06:37:19 +0200

    added pipeline

--- CommonExtensions/Common.cs
+++ CommonExtensions/Common.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CommonExtensions
+﻿namespace CommonExtensions
 {
     public static class Common
     {
