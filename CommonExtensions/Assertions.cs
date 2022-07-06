b4f584f45df5ce1bac71d05fc602016bdc522118
Lukas Nagel <lukas.nagel04@protonmail.com>
2022-07-06 07:46:14 +0200

    changed to .netstandard

---
+++ CommonExtensions/Assertions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonExtensions
+{
+    public static class Assertions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> elements)
+        {
+            if (elements == null || elements.Count() == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- CommonExtensions/Common.cs
+++ CommonExtensions/Common.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CommonExtensions
 {
@@ -45,16 +41,6 @@ namespace CommonExtensions
             }
 
             return result;
-        }
-
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> elements)
-        {
-            if (elements == null || elements.Count() == 0)
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
