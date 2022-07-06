a9dfd6b224aaa0f499b25cb3aca8cc035c32a687
Lukas Nagel <lukas.nagel04@protonmail.com>
2022-07-06 12:52:49 +0200

    added test

---
+++ CommonExtensions.Test/TextShould.cs
@@ -0,0 +1,18 @@
+﻿namespace CommonExtensions.Test
+{
+    public class TextShould
+    {
+        [Fact]
+        public void SplitToString()
+        {
+            //Arrange 
+            string[] input = { "Test1", "Test2", "Test3" };
+
+            //Act
+            string result = input.SplitToString(", ");
+
+            //Assert
+            Assert.Equal("Test1, Test2, Test3", result);
+        }
+    }
+}
