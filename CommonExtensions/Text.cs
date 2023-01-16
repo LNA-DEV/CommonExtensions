51bf9da88745e91cd148aa97ba261f262b2b0ced
Lukas Nagel <lukas@lna-dev.net>
2023-01-16 21:09:11 +0100

    📝 Updated Docs

--- CommonExtensions/Text.cs
+++ CommonExtensions/Text.cs
@@ -2,8 +2,16 @@
 
 namespace CommonExtensions
 {
+    /// <summary>
+    ///     Extension Methods which topic operations based on Text.
+    /// </summary>
     public static class Text
     {
+        /// <summary>
+        ///     An extension method which converts an string into an Camel-Case string.
+        /// </summary>
+        /// <param name="s">String which will be converted.</param>
+        /// <returns>The method will return a Camel-Case string.</returns>
         public static string ToCamelCase(this string s)
         {
             var result = char.ToLower(s[0]) + s.Substring(1);
@@ -11,11 +19,17 @@ namespace CommonExtensions
             return result;
         }
 
+        /// <summary>
+        ///     This method splits an IEnumerable of strings into an single string seperated with the separator string.
+        /// </summary>
+        /// <param name="strings">The strings which will be converted into a string.</param>
+        /// <param name="separator">Separator string which will split the values.</param>
+        /// <returns>A with the separator seperated string.</returns>
         public static string SplitToString(this IEnumerable<string> strings, string separator)
         {
             var result = "";
 
-            if (strings != null)
+            if (strings.IsNotNullOrEmpty())
             {
                 foreach (var item in strings) result += item + separator;
 
