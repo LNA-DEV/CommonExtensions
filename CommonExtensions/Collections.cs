8ab5fb9f6713e38b37dbb4492d9c6651a790c7f3
Lukas Nagel <lukas@lna-dev.net>
2023-04-29 20:16:31 +0200

    🐛 Updated IsNullOrEmpty

--- CommonExtensions/Collections.cs
+++ CommonExtensions/Collections.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace CommonExtensions
 {
+    /// <summary>
+    ///     This class contains the extension methods for collections.
+    /// </summary>
     public static class Collections
     {
         /// <summary>
         ///     Returns TRUE if an IEnumerable is NULL or empty
         /// </summary>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> elements)
+        public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? elements)
         {
             return elements.IsNull() || !elements.Any();
         }
@@ -16,7 +20,7 @@ namespace CommonExtensions
         /// <summary>
         ///     Returns FALSE if an IEnumerable is NULL or empty
         /// </summary>
-        public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> elements)
+        public static bool IsNotNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? elements)
         {
             return !elements.IsNullOrEmpty();
         }
